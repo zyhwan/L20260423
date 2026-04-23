f35802eccb25cf9470797e7525b6de288648cb0d
zyhwan <zyhwan010410@naver.com>
2026-04-23 09:04:36 +0900

    Initial commit

---
+++ Source/L20260423/GM_Basic.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GM_Basic.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class L20260423_API AGM_Basic : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/L20260423/L20260423.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "L20260423.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, L20260423, "L20260423" );
